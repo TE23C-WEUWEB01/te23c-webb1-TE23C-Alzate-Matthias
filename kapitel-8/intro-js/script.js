1d479e1041e013b681c911dfe1c6eb1dea32f389
Matthias Alzate <matthias.alzate@elev.ga.ntig.se>
2025-03-13 14:26:30 +0100

    tränat på javascript, variabler, konkatenering

---
+++ kapitel-8/intro-js/script.js
@@ -0,0 +1,17 @@
+// var tal1 = 5;
+// var tal2 = 2;
+// var summa = tal1 + tal2;
+// alert(tal1 + " + " + tal2 + " = " + summa);
+
+var förnamn = "Matthias";
+var efternamn =  "Alzate";
+var ålder = 17;
+var namn = förnamn + " " + efternamn;
+// var meddelande = namn + " är " + ålder + " år gammal.";
+// alert(meddelande);
+
+var hobby = "spela datorspel";
+var kostnad = 1000;
+var meddelande = namn + " gillar att " + hobby + " och det kostar " + kostnad + "kr per år";
+// T.ex Karim Ryde gillar segling och det kostar ?? per år.
+alert(meddelande);
