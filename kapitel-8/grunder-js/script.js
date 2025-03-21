e62bb0c92ae9f54dcb5928b4862afcd7d1291c68
Matthias Alzate <matthias.alzate@elev.ga.ntig.se>
2025-03-21 09:43:58 +0100

    tränat på if-satser och loopar

---
+++ kapitel-8/grunder-js/script.js
@@ -0,0 +1,41 @@
+// här skriver js-kod
+
+// var tal1 = parseInt(prompt("Skriv in ett heltal: "))
+// var tal2 = parseInt(prompt("Skriv in ett till heltal: "))
+
+// if (tal1 > tal2) {
+//    document.writeln(`${tal1} är större än ${tal2}`)
+//}
+//else {
+//    document.writeln(`${tal2} är större än ${tal1}`)
+//}
+
+// en liten inloggning, dvs fråga efter konto, lösenord och gemför med valbart lösenort
+// svara att "Du är inloggad" annars "Fel lösenord eller konto"
+// var konto = "Matthias"
+// var lösenord = "SillyLittleGuy"
+
+// while (true) {
+//     var svar1 = prompt("Skriv in ditt användarnamn")
+//     var svar2 = prompt("Skriv in ditt lösenord")
+
+//     if (svar1 == konto && svar2 == lösenord) {
+//         document.writeln("Du är inloggad")
+//         break
+//     } else {
+//         document.writeln("Fel lösenord eller konto")
+//     }
+// }
+
+
+// Skriv ut talet 20-30
+
+// for (let index = 20; index < 31; index++) {
+//     document.writeln(`tal: ${index} <br>`)
+// }
+
+let val
+
+while (val) {
+    val = confirm("klicka OK för att fortsätta!") // true eller false
+}
